6b45436cdcdde7d2a189f77907aee03d412fa99d
shaw <jind.shaw@outlook.com>
2023-06-14 14:38:16 +0800

    init timer project

---
+++ timer/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.filePathText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectFile = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.ListBox();
+            this.clearMessage = new System.Windows.Forms.Button();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件：";
+            // 
+            // filePathText
+            // 
+            this.filePathText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filePathText.Location = new System.Drawing.Point(111, 25);
+            this.filePathText.Name = "filePathText";
+            this.filePathText.Size = new System.Drawing.Size(306, 21);
+            this.filePathText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "执行间隔(小时)：";
+            // 
+            // selectFile
+            // 
+            this.selectFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectFile.Location = new System.Drawing.Point(423, 25);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(62, 23);
+            this.selectFile.TabIndex = 4;
+            this.selectFile.Text = "选取文件";
+            this.selectFile.UseVisualStyleBackColor = true;
+            this.selectFile.Click += new System.EventHandler(this.selectFile_Click);
+            // 
+            // start
+            // 
+            this.start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.start.Location = new System.Drawing.Point(287, 73);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(62, 23);
+            this.start.TabIndex = 5;
+            this.start.Text = "开始";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop.Location = new System.Drawing.Point(355, 72);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(62, 23);
+            this.stop.TabIndex = 6;
+            this.stop.Text = "停止";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // message
+            // 
+            this.message.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.message.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.message.FormattingEnabled = true;
+            this.message.ItemHeight = 20;
+            this.message.Location = new System.Drawing.Point(7, 109);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(478, 304);
+            this.message.TabIndex = 8;
+            // 
+            // clearMessage
+            // 
+            this.clearMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearMessage.Location = new System.Drawing.Point(423, 73);
+            this.clearMessage.Name = "clearMessage";
+            this.clearMessage.Size = new System.Drawing.Size(62, 23);
+            this.clearMessage.TabIndex = 9;
+            this.clearMessage.Text = "清除";
+            this.clearMessage.UseVisualStyleBackColor = true;
+            this.clearMessage.Click += new System.EventHandler(this.clearMessage_Click);
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown.DecimalPlaces = 1;
+            this.numericUpDown.Location = new System.Drawing.Point(111, 73);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(170, 21);
+            this.numericUpDown.TabIndex = 10;
+            this.numericUpDown.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 436);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.clearMessage);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.filePathText);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox filePathText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button selectFile;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.ListBox message;
+        private System.Windows.Forms.Button clearMessage;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+    }
+}
+
